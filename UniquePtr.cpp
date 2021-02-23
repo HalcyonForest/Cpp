36ff7dcd2184bab0efb47e921e95b52c8116930a
Egor Tkachenko <68852747+HalcyonForest@users.noreply.github.com>
2021-02-23 13:39:53 +0300

    Add files via upload

---
+++ UniquePtr.cpp
@@ -0,0 +1,83 @@
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+
+template<class T>
+class Unique_ptr {
+private:
+    T *ptr;
+public:
+    Unique_ptr(T *ptr = nullptr) : ptr(ptr) {}
+
+    Unique_ptr(Unique_ptr<T> &&oldPtr) {
+        this->ptr = move(oldPtr.ptr);
+        oldPtr.ptr = nullptr;
+    }
+
+    Unique_ptr &operator=(Unique_ptr<T> &oldPtr) {
+        if (this == &oldPtr)
+            return *this;
+        delete ptr;
+        ptr = oldPtr.ptr;
+        oldPtr.ptr = nullptr;
+        return *this;
+    }
+
+    T &operator*() {
+        return *this->ptr;
+    }
+
+    T *operator->() {
+        return &this->ptr;
+    }
+
+    ~Unique_ptr() {
+        delete ptr;
+    }
+};
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <math.h>
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+
+template<class T>
+class Unique_ptr {
+private:
+    T *ptr;
+public:
+    Unique_ptr(T *ptr = nullptr) : ptr(ptr) {}
+
+    Unique_ptr(Unique_ptr<T> &&oldPtr) {
+        this->ptr = move(oldPtr.ptr);
+        oldPtr.ptr = nullptr;
+    }
+
+    Unique_ptr &operator=(Unique_ptr<T> &oldPtr) {
+        if (this == &oldPtr)
+            return *this;
+        delete ptr;
+        ptr = oldPtr.ptr;
+        oldPtr.ptr = nullptr;
+        return *this;
+    }
+
+    T &operator*() {
+        return *this->ptr;
+    }
+
+    T *operator->() {
+        return &this->ptr;
+    }
+
+    ~Unique_ptr() {
+        delete ptr;
+    }
+};
